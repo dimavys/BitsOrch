59486733250b15c64f3a441c01e7d493ca801420
Dmitriy <dimashkavysotskyy@gmail.com>
2023-03-11 12:28:41 +0200

    init commit

---
+++ src/BitsOrch.Infrastructure/Entities/FieldEntity.cs
@@ -0,0 +1,14 @@
+namespace BitsOrch.Infrastructure.Entities;
+
+public class FieldEntity
+{
+    public string Name { get; set; }
+
+    public DateTime BirthDate { get; set; }
+
+    public bool Married { get; set; }
+    
+    public string MobilePhone { get; set; }
+    
+    public decimal Salary { get; set; }
+}
